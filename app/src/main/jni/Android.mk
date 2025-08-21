3772cd20b3dd92ad5b20aafc2fe31808e3bfddf1
TolstoffGame <tolstoffgame24@gmail.com>
2025-08-22 01:46:32 +1000

    Android SDL project + engine sources + CI

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -8,7 +8,7 @@ LOCAL_LDLIBS := -llog -landroid -lGLESv2 -lEGL -lm
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image
 include $(BUILD_SHARED_LIBRARY)
 
-# SDL2
+# SDL2 (папка SDL лежит в соседнем модуле android-project)
 $(call import-module,SDL)
-# SDL2_image
+# SDL_image (мы только что склонировали в jni/SDL_image)
 $(call import-module,SDL_image)
