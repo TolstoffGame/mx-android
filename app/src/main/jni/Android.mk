72092947274f1174b9b69cf31262632a399053f4
TolstoffGame <tolstoffgame24@gmail.com>
2025-08-22 02:34:56 +1000

    NDK build: Android.mk+Application.mk, vendored SDL2/SDL2_image

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,4 +1,6 @@
 LOCAL_PATH := $(call my-dir)
+# Чтобы $(call import-module,...) искал модули внутри текущего каталога jni
+NDK_MODULE_PATH := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := main
@@ -8,7 +10,6 @@ LOCAL_LDLIBS := -llog -landroid -lGLESv2 -lEGL -lm
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image
 include $(BUILD_SHARED_LIBRARY)
 
-# SDL2 (папка SDL лежит в соседнем модуле android-project)
-$(call import-module,SDL)
-# SDL_image (мы только что склонировали в jni/SDL_image)
-$(call import-module,SDL_image)
+# Импортируем модули, которые мы положили как jni/SDL2 и jni/SDL2_image
+$(call import-module,SDL2)
+$(call import-module,SDL2_image)
